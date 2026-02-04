24a02dfbb83bec14118076b9648f5d8b87b74ca7
perhapslaksh <perhapslaksh@gmail.com>
2026-02-04 15:02:12 +0530

    fix: correct Button Slot usage

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -49,7 +49,7 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
   }) {
-  const Comp = asChild ? Slot.Root : "button"
+  const Comp = asChild ? Slot : "button"
 
   return (
     <Comp
@@ -61,5 +61,6 @@ function Button({
     />
   )
 }
+
 
 export { Button, buttonVariants }
